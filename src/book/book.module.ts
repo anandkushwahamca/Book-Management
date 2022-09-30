497585b566fba0f5ac9a1c7d1122b0443d265542
anandkushwahamca <anand.kushwaha49@gmail.com>
2022-09-30 13:19:28 +0530

    Implemented middleware in book module

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,13 +1,31 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookController } from './book.controller';
 import { Book } from './book.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Author } from '../author/author.entity';
+import { LoggerMiddleware } from '../common/middleware/logger.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Book, Author])],
   controllers: [BookController],
   providers: [BookService],
 })
-export class BookModule {}
+export class BookModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(
+        { path: 'book', method: RequestMethod.GET },
+        { path: 'book', method: RequestMethod.POST },
+        { path: 'book/:id', method: RequestMethod.PUT },
+        { path: 'book/:id', method: RequestMethod.DELETE },
+        { path: 'book/bookTitle/:title', method: RequestMethod.GET },
+      );
+  }
+}
--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,10 +1,11 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request...');
+  private readonly logger = new Logger(LoggerMiddleware.name);
+  use(req: Request, res: Response, next: (error?: NextFunction) => void) {
+    this.logger.log(LoggerMiddleware.name);
     next();
   }
 }
